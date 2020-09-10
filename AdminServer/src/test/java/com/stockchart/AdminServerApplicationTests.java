f4c1033fe96ec6c8f471cccd77c1e9ff0deb17ac
pg-sooraj <70253923+pg-sooraj@users.noreply.github.com>
2020-09-10 10:15:22 +0530

    Add files via upload

---
+++ AdminServer/src/test/java/com/stockchart/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
